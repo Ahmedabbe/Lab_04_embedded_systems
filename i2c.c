241824409d035b8d9ee7a2ac76bf049d7bdee789
Ahmed <70702026+Ahmedabbe@users.noreply.github.com>
2021-11-16 22:47:45 +0100

    Deluppgift 1
    KONFIGURERA OCH ANVÄND I2C GENERELLT

--- i2c.c
+++ i2c.c
@@ -10,9 +10,9 @@
 void i2c_init(void)
 {
 	/*
-		Set TWEN bit in TWCR reg = enable TWI(Two Wire Interface)
-		Set TWSR reg byte to 0x00 = prescaler of 1
-		Set TWBR reg byte to 0x48 = sets bit rate to 100 kHz
+		-Set TWEN bit in TWCR reg = enable TWI(Two Wire Interface)
+		-Set TWSR reg byte to 0x00 = prescaler of 1
+		-Set TWBR reg byte to 0x48 = sets bit rate to 100 kHz
 		Calculation for TWBR:
 			SCLfreq = F_CPU / (16 + (2 * TWBR * Prescaler))
 			100 000 = 16 000 000 / (16 + (2 * TWBR * 1)
@@ -72,10 +72,10 @@ void i2c_meaningful_status(uint8_t status)
 inline void i2c_start()
 {
 	/*
-		Set TWEN bit in TWCR reg = activate SCL/SDA pins and enable TWI operations
-		TWSTA bit set in TWCR register to generate start condition, will be cleared in i2c_xmit_addr
-		TWINT bit set in TWCR register clears the flag and starts the operation of TWI
-		Await until TWI has finished its current job AKA TWINT bit is set
+		-Set TWINT bit in TWCR reg = clears flag and starts TWI operation
+		-Set TWSTA bit in TWCR reg = generate start condition. Is cleared in function "i2c_xmit_addr"
+		-Set TWEN bit in TWCR reg = activate SCL/SDA pins and enable TWI operations
+			wait for TWINT bit to be set
 	*/
 	TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
 
@@ -85,6 +85,13 @@ inline void i2c_start()
 
 inline void i2c_stop()
 {
+	/*
+		-Set TWINT bit in TWCR reg = clears flag and starts TWI operation
+		-Set TWSTO bit in TWCR reg = generate stop condition. Is automatically cleared after executing
+		-Set TWEN bit in TWCR reg = activate SCL/SDA pins and enable TWI operations
+			wait for TWSTO bit to clear
+	*/
+
 	TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);
 
 	while ((TWCR & (1 << TWSTO)))
@@ -93,6 +100,9 @@ inline void i2c_stop()
 
 inline uint8_t i2c_get_status(void)
 {
+	/*
+
+	*/
 	uint8_t status;
 	status = TWSR & 0xF8;
 
@@ -101,7 +111,17 @@ inline uint8_t i2c_get_status(void)
 
 inline void i2c_xmit_addr(uint8_t address, uint8_t rw)
 {
-	TWDR = (address & 0xfe) | (rw & 0x01);
+	/*
+		In Transmit mode, TWDR contains the next byte to be transmitted. 
+		In Receive mode, the TWDR contains the last byte received.
+
+		-Assign eeprom adress followed by read/write bit to TWDR register
+		-Set TWINT bit in TWCR reg = clears flag and starts TWI operation
+		-Set TWEN bit in TWCR reg = activate SCL/SDA pins and enable TWI operations
+			wait for TWINT bit to be set
+	*/
+
+	TWDR = (eeprom_addr & 0xfe) | (i2c_rw & 0x01);
 
 	TWCR = (1 << TWINT) | (1 << TWEN);
 
@@ -111,7 +131,15 @@ inline void i2c_xmit_addr(uint8_t address, uint8_t rw)
 
 inline void i2c_xmit_byte(uint8_t data)
 {
-	TWDR = data;
+	/*
+		Assign next byte to transmit to TWDR
+
+		-Set TWINT bit in TWCR reg = clears flag and starts TWI operation
+		-Set TWEN bit in TWCR reg = activate SCL/SDA pins and enable TWI operations
+			wait for TWINT bit to be set
+	*/
+
+	TWDR = byte;
 
 	TWCR = (1 << TWINT) | (1 << TWEN);
 
@@ -121,6 +149,15 @@ inline void i2c_xmit_byte(uint8_t data)
 
 inline uint8_t i2c_read_ACK()
 {
+	/*
+		-Set TWINT bit in TWCR reg = clears flag and starts TWI operation
+		-Set TWEN bit in TWCR reg = activate SCL/SDA pins and enable TWI operations
+		-Set TWEA bit in TWCR reg = generates ACK pulse on the TWI bus
+			wait for TWINT bit to be set
+		
+		return received data byte
+	*/
+
 	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWEA);
 
 	while (!(TWCR & (1 << TWINT)))
@@ -131,6 +168,14 @@ inline uint8_t i2c_read_ACK()
 
 inline uint8_t i2c_read_NAK()
 {
+	/*
+		-Set TWINT bit in TWCR reg = clears flag and starts TWI operation
+		-Set TWEN bit in TWCR reg = activate SCL/SDA pins and enable TWI operations
+			wait for TWINT bit to be set and no ACK (NAK) pulse confirmed
+
+		return received data byte
+	*/
+
 	TWCR = (1 << TWINT) | (1 << TWEN);
 
 	while (!(TWCR & (1 << TWINT)))
@@ -141,84 +186,25 @@ inline uint8_t i2c_read_NAK()
 
 inline void eeprom_wait_until_write_complete()
 {
-	while (i2c_get_status() != 0x18)
-	{
-		i2c_start();
-		i2c_xmit_addr(EEPROM_ADDR, I2C_W);
-	}
+	// ...
 }
 
 uint8_t eeprom_read_byte(uint8_t addr)
 {
-	uint8_t readByte;
-
-	i2c_start();
-	i2c_xmit_addr(EEPROM_ADDR, I2C_W);
-	i2c_xmit_byte(addr);
-
-	i2c_start();
-	i2c_xmit_addr(EEPROM_ADDR, I2C_R);
-	readByte = i2c_read_NAK();
-
-	i2c_stop();
-
-	return readByte;
+	// ...
 }
 
 void eeprom_write_byte(uint8_t addr, uint8_t data)
 {
-	i2c_start();
-	i2c_xmit_addr(EEPROM_ADDR, I2C_W);
-
-	i2c_xmit_byte(addr);
-	i2c_xmit_byte(data);
-
-	i2c_stop();
-	eeprom_wait_until_write_complete();
+	// ...
 }
 
-void eeprom_write_page(uint8_t addr, char *data)
+void eeprom_write_page(uint8_t addr, uint8_t *data)
 {
-	while(addr % 8 != 0){ 
-		addr++;
-	}
-	
-	i2c_start();
-	i2c_xmit_addr(EEPROM_ADDR, I2C_W);
-	i2c_xmit_byte(addr);
-
-	for (int i = 0; i < strlen(data); i++)
-	{
-		i2c_xmit_byte(data[i]);
-	}
-
-	i2c_stop();
-	eeprom_wait_until_write_complete;
+	// ... (VG)
 }
 
-void eeprom_sequential_read(char *buf, uint8_t start_addr, uint8_t len)
+void eeprom_sequential_read(uint8_t *buf, uint8_t start_addr, uint8_t len)
 {
-	while(start_addr % 8 != 0){
-		start_addr++;
-	}
-
-	i2c_start();
-	i2c_xmit_addr(EEPROM_ADDR, I2C_W);
-	printf_P(PSTR("xmit addr \n"));
-	i2c_xmit_byte(start_addr);
-	printf_P(PSTR("xmit byte \n"));
-
-	i2c_start();
-	i2c_xmit_addr(EEPROM_ADDR, I2C_R);
-	printf_P(PSTR("xmit word addr \n"));
-
-	for (int i = 0; i < (len - 1); i++)
-	{
-		buf[i] = i2c_read_ACK();
-	}
-	printf_P(PSTR("ACK \n"));
-	buf[len - 1] = i2c_read_NAK();
-	printf_P(PSTR("NAK \n"));
-	i2c_stop();
-	printf_P(PSTR("stop \n"));
+	// ... (VG)
 }
